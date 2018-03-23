8c7916a051951e78cac16ff6206f08a96f351ecc
Brad Busse <bbusse@gmail.com>
2018-03-23 14:19:04 -0400

    Add commented breakdown into real and imaginary components

--- autoencode.py
+++ autoencode.py
@@ -8,7 +8,10 @@ import numpy as np
 def f2m(freq): #frequency to melody scale
     return 1127*np.log(1+freq/700.)
 
-song = AudioSegment.from_mp3('Stellardrone - Between The Rings - 01 To The Great Beyond.mp3')
+mp3 = 'Stellardrone - Between The Rings - 01 To The Great Beyond.mp3'
+#mp3 = '../13 - Laika.mp3'
+
+song = AudioSegment.from_mp3(mp3)
 song.export('temp.wav',format='wav')
 
 rate, audio = scipy.io.wavfile.read('temp.wav')
@@ -22,6 +25,8 @@ left_ = audio[:,0]
 #f, t, Sxx = scipy.signal.spectrogram(left_,rate/40.,nperseg=1024,noverlap=512,return_onesided=True)
 f, t, Zxx = scipy.signal.stft(left_, rate, nperseg=1024)
 
+#Zxx = np.real(Zxx)
+#Zxx = np.imag(Zxx)
 #Zxx = np.where(np.abs(Zxx) >= .001, Zxx, 0)
 
 t2, x = scipy.signal.istft(Zxx,rate)#,nperseg=2048)
@@ -29,7 +34,7 @@ t2, x = scipy.signal.istft(Zxx,rate)#,nperseg=2048)
 scipy.io.wavfile.write('out.wav',rate,x.astype(np.int16))
 
 print Zxx.shape
-
+#print Zxx[:,0]
 #print f, Zxx
 print 'Max mel:',f2m(Zxx.shape[1])
 
